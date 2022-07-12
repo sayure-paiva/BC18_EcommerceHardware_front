89cfc5e6386051e55fd130081fc23e2f3814a28a
Elainebritodev <elainebrito.dev@gmail.com>
2022-07-11 22:25:46 -0300

    Formuário da Página de Recuperar Senha com validação e captcha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './auth/cadastro/cadastro.component';
+import { RecuperarSenhaComponent } from './auth/recuperar-senha/recuperar-senha.component';
 import { NotFoundComponent } from './core/components/not-found/not-found.component';
 import { TermoDePrivacidadeComponent } from './core/components/termo-de-privacidade/termo-de-privacidade.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'cadastro', component: CadastroComponent},
-  {path: 'termo-de-privacidade', component: TermoDePrivacidadeComponent},
-  {path: '**', component: NotFoundComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'recuperar-senha', component: RecuperarSenhaComponent },
+  { path: 'termo-de-privacidade', component: TermoDePrivacidadeComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
--- src/app/auth/recuperar-senha/recuperar-senha.component.ts
+++ src/app/auth/recuperar-senha/recuperar-senha.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recuperar-senha',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarSenhaComponent implements OnInit {
 
-  constructor() { }
+  siteKey: string;
+
+  constructor(private fb: FormBuilder) {
+    this.siteKey = '6LfZoeIgAAAAAL36fd8Z62_r1rUqdz1g4VSgxqDz';
+  }
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    recaptcha: ['', Validators.required]
+  });
+
+  ocultar = true;
+
+
+  onSubmit() {
+    alert('E-mail enviado com sucesso!')
+  }
 
   ngOnInit(): void {
   }
+
 
 }
